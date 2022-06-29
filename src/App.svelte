781f849d65ca600eb224ad8971ba5965b7cc4109
William Faircloth <willuhmjs@gmail.com>
2022-06-29 19:00:28 +0000

    Change Bootstrap.svelte to Header.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import SwiftieLyric from './lib/SwiftieLyric.svelte'
+  import Header from './lib/Header.svelte';
 </script>
 
+<Header/>
 <main>
   <p>You can't spell awesome without me!</p>
   <SwiftieLyric />
--- src/lib/SwiftieLyric.svelte
+++ src/lib/SwiftieLyric.svelte
@@ -1,12 +1,12 @@
 <script>
   import { Lyrics } from "./SwiftieLyricList.svelte";
-  import Bootstrap from "./Bootstrap.svelte";
+  import Header from "./Header.svelte";
   let SwiftieLyric = "Swiftie Lyric";
   const randomSwiftieLyric = () => {
     SwiftieLyric = Lyrics[Math.floor(Math.random() * Lyrics.length)];
   }
 </script>
 
-<Bootstrap/>
+<Header/>
 <p>{SwiftieLyric}</p>
 <button on:click={randomSwiftieLyric}>Random Swiftie Lyric</button>
