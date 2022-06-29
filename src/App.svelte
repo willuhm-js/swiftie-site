c38d6b049c38c8bc4cb0f817e8785e85788aa695
William Faircloth <willuhmjs@gmail.com>
2022-06-29 19:05:41 +0000

    Restructuring

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,15 @@
 
 <Header/>
 <main>
+  <h1>swiftie</h1>
   <p>You can't spell awesome without me!</p>
   <SwiftieLyric />
 </main>
 
 <style>
-  
+   @font-face { font-family: TaylorSwift; src: url('TaylorSwift.ttf'); }
+   h1 {
+        font-family: TaylorSwift;
+        text-align: center;
+   } 
 </style>
