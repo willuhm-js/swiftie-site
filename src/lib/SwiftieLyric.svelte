3c5f28a97fad7c9da0b85f77e671655af69c866f
William Faircloth <willuhmjs@gmail.com>
2022-07-01 00:59:53 +0000

    Display the lyrics and an image

--- src/lib/SwiftieLyric.svelte
+++ src/lib/SwiftieLyric.svelte
@@ -1,24 +1,34 @@
 <script lang="ts">
-  import { Lyrics } from "./SwiftieLyricList.svelte";
-  let song, lyric;
+  import { listen } from "svelte/internal";
+import { Songs } from "./SwiftieLyricList.svelte";
+  let song, stanza, image;
+  
+  const randomIndex = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
   const randomSwiftieLyric = () => {
-    let LyricsObject = Lyrics[Math.floor(Math.random() * Lyrics.length)];
-    song = LyricsObject.song;
-    lyric = LyricsObject.lyric.split("\n");
+    let SongObject = randomIndex(Songs)
+    let stanzaArray = SongObject.lyrics.split("\n\n");
+    stanza = randomIndex(stanzaArray).split("\n");
+    if (stanza.length < 3 || stanza.length > 5) return randomSwiftieLyric();
+    song = SongObject.songTitle;
+    image = SongObject.coverArt
   }
   randomSwiftieLyric();
 </script>
 
-<figure class="p-4 w-100 rounded">
-  <blockquote class="blockquote text-light">
+<figure class="p-4 w-100 rounded" >
+  <blockquote class="blockquote text-light" style="min-height: 200px;">
+  <img src="{image}" alt="Album cover" width="200px" style="float: left;"/>
     <p>
-      {#each lyric as oneLyric}
-      <span>{oneLyric}<br></span>
+      {#each stanza as line}
+        {#if line.match(/\w/)}
+        <span>{line}<br></span>
+        {/if}
       {/each}
     </p>
   </blockquote>
   <figcaption class="blockquote-footer mb-2">
-    Taylor Swift in <cite title="Source Title">{song}</cite>
+   <cite title="Source Title">{song}</cite> by Taylor Swift
   </figcaption>
 </figure>
 <button on:click={randomSwiftieLyric}>Random Swiftie Lyric</button>
